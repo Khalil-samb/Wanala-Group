96f7fdbd615ff7c1300c225c3ca88b7b1b4aab40
BigLineDev0 <alioudiallo.py@gmail.com>
2026-08-06 15:44:52 +0000

    mise a jour fichier setting

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)@xcvkze$(y(nkdv)(6euqy3z52$17vfer3x9ovupf^h)^=(nj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["wanala-group.onrender.com"]
 
 
 # Application definition
